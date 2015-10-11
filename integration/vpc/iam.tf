9ac606e25e073466871b62c7097ef81a55d7554a
Tomas Doran <tdoran@yelp.com>
2015-10-11 22:21:36 +0100

    Add ec2:Describe*

--- integration/vpc/iam.tf
+++ integration/vpc/iam.tf
@@ -21,3 +21,22 @@ resource "aws_iam_role" "role" {
 EOF
 }
 
+resource "aws_iam_role_policy" "test_policy" {
+    name = "test_policy"
+    role = "${aws_iam_role.role.id}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
