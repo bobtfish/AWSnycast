9c7742cffdee508e21613070936fbb91e764f1e4
Tomas Doran <tdoran@yelp.com>
2015-10-15 23:44:35 +0100

    Allow CreateRoute and ReplaceRoute

--- tests/integration/iam.tf
+++ tests/integration/iam.tf
@@ -21,8 +21,8 @@ resource "aws_iam_role" "role" {
 EOF
 }
 
-resource "aws_iam_role_policy" "test_policy" {
-    name = "test_policy"
+resource "aws_iam_role_policy" "describe_instances" {
+    name = "describe_instances"
     role = "${aws_iam_role.role.id}"
     policy = <<EOF
 {
@@ -36,6 +36,27 @@ resource "aws_iam_role_policy" "test_policy" {
       "Resource": "*"
     }
   ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "modify_routes" {
+    name = "modify_routes"
+    role = "${aws_iam_role.role.id}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+        {
+            "Action": [
+                "ec2:ReplaceRoute",
+                "ec2:CreateRoute",
+                "ec2:DescribeRouteTables"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ]
 }
 EOF
 }
