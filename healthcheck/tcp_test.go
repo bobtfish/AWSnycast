a0a4efc0dd1d40873b04a45fada309f2644a5a04
Tomas Doran <tdoran@yelp.com>
2015-10-21 15:48:23 +0100

    Test healthchecking a closed port

--- healthcheck/tcp_test.go
+++ healthcheck/tcp_test.go
@@ -158,3 +158,38 @@ func TestHealthcheckTcpFail(t *testing.T) {
 	quit = true
 	ln.Close()
 }
+
+func TestHealthcheckTcpClosed(t *testing.T) {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	port := ln.Addr().(*net.TCPAddr).Port
+	ln.Close() // Close the port again before running healthcheck
+	c := make(map[string]string)
+	c["port"] = fmt.Sprintf("%d", port)
+	h := Healthcheck{
+		Type:        "tcp",
+		Destination: "127.0.0.1",
+		Config:      c,
+	}
+	h.Default()
+	err = h.Validate("foo")
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	err = h.Setup()
+	if err != nil {
+		t.Log("Setup failed: %s", err.Error())
+		t.Fail()
+	} else {
+		log.Printf("%+v", h)
+		res := h.healthchecker.Healthcheck()
+		if res {
+			t.Log("h.healthchecker.Healthcheck() returned OK for closed port")
+			t.Log(fmt.Sprintf("%+v", res))
+			t.Fail()
+		}
+	}
+}
