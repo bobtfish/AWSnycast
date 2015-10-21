60c85239adc1c7425b434551a4e53cdf1fda7968
Tomas Doran <tdoran@yelp.com>
2015-10-21 15:54:25 +0100

    Test clients which close straight away

--- healthcheck/tcp_test.go
+++ healthcheck/tcp_test.go
@@ -193,3 +193,56 @@ func TestHealthcheckTcpClosed(t *testing.T) {
 		}
 	}
 }
+
+func TestHealthcheckTcpFailClientClose(t *testing.T) {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	port := ln.Addr().(*net.TCPAddr).Port
+	t.Log(fmt.Sprintf("%+v", ln))
+	ready := make(chan bool, 1)
+	quit := false
+	go func() {
+		for {
+			ready <- true
+			conn, err := ln.Accept()
+			if err != nil {
+				if quit {
+					return
+				}
+				t.Fatal(fmt.Printf("Error accepting: %s", err.Error()))
+			}
+			conn.Close() // Client closes connection straight away, before reading anything
+		}
+	}()
+	<-ready
+	c := make(map[string]string)
+	c["port"] = fmt.Sprintf("%d", port)
+	h := Healthcheck{
+		Type:        "tcp",
+		Destination: "127.0.0.1",
+		Config:      c,
+	}
+	h.Default()
+	err = h.Validate("foo")
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+	err = h.Setup()
+	if err != nil {
+		t.Log("Setup failed: %s", err.Error())
+		t.Fail()
+	} else {
+		log.Printf("%+v", h)
+		res := h.healthchecker.Healthcheck()
+		if res {
+			t.Log("h.healthchecker.Healthcheck() returned OK for client close before send")
+			t.Log(fmt.Sprintf("%+v", res))
+			t.Fail()
+		}
+	}
+	quit = true
+	ln.Close()
+}
