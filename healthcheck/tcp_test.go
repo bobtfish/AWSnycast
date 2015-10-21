581699776f04d352d0a3641e0b702ffd5ac4050c
Tomas Doran <tdoran@yelp.com>
2015-10-21 15:46:33 +0100

    Failing healthcheck test

--- healthcheck/tcp_test.go
+++ healthcheck/tcp_test.go
@@ -93,22 +93,68 @@ func TestHealthcheckTcp(t *testing.T) {
 	ln.Close()
 }
 
-/*
 func TestHealthcheckTcpFail(t *testing.T) {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	port := ln.Addr().(*net.TCPAddr).Port
+	t.Log(fmt.Sprintf("%+v", ln))
+	ready := make(chan bool, 1)
+	quit := false
+	go func() {
+		for {
+			ready <- true
+			conn, err := ln.Accept()
+			if err != nil {
+				if quit {
+					return
+				}
+				t.Fatal(fmt.Printf("Error accepting: %s", err.Error()))
+			}
+			go func(conn net.Conn) {
+				buf := make([]byte, 1024)
+				n, err := conn.Read(buf)
+				if err != nil {
+					t.Log(fmt.Printf("Error reading: %s", err.Error()))
+					t.Fail()
+				}
+				if string(buf[:n]) != "HEAD / HTTP/1.0\r\n\r\n" {
+					t.Log(string(buf[:n]))
+					t.Fail()
+				}
+				conn.Write([]byte("500 OOPS"))
+				conn.Close()
+			}(conn)
+		}
+	}()
+	<-ready
+	c := make(map[string]string)
+	c["port"] = fmt.Sprintf("%d", port)
 	h := Healthcheck{
 		Type:        "tcp",
-		Destination: "169.254.255.45", // Hopefully you can't talk to this :)
+		Destination: "127.0.0.1",
+		Config:      c,
 	}
 	h.Default()
-	err := h.Validate("foo")
+	err = h.Validate("foo")
 	if err != nil {
 		t.Log(err)
 		t.Fail()
 	}
-	h.Setup()
-	res := h.healthchecker.Healthcheck()
-	if res {
+	err = h.Setup()
+	if err != nil {
+		t.Log("Setup failed: %s", err.Error())
 		t.Fail()
+	} else {
+		log.Printf("%+v", h)
+		res := h.healthchecker.Healthcheck()
+		if res {
+			t.Log("h.healthchecker.Healthcheck() returned OK for a 500")
+			t.Log(fmt.Sprintf("%+v", res))
+			t.Fail()
+		}
 	}
+	quit = true
+	ln.Close()
 }
-*/
