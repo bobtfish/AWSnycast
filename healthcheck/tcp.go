cf898d8c243db93620e9fa2cbdd5ead73d1d1636
Tomas Doran <tdoran@yelp.com>
2015-10-22 18:40:55 +0100

    Make that test pass

--- healthcheck/tcp.go
+++ healthcheck/tcp.go
@@ -27,15 +27,21 @@ func (h TcpHealthCheck) Healthcheck() bool {
 	//
 	c, err := net.Dial("tcp", h.Destination+":"+h.Port)
 	if err != nil {
-		log.Println("net.Dial: %s", err)
+		log.Println("net.Dial: %s", err.Error())
 		return false
 	}
 	defer c.Close()
 	c.SetDeadline(time.Now().Add(time.Second * 10))
 
-	fmt.Fprintf(c, h.Send)
-	// Read 1 byte from the TCP connection.
-	//
+	_, err = fmt.Fprintf(c, h.Send)
+	if err != nil {
+		log.Println("TCP send: %s", err.Error())
+	}
+
+	if h.Expect == "" {
+		return true
+	}
+
 	b := make([]byte, 1024)
 	n, err := c.Read(b)
 	if err != nil {
@@ -43,12 +49,8 @@ func (h TcpHealthCheck) Healthcheck() bool {
 		return false
 	}
 	answer := string(b[:n])
-	if h.Expect == "" {
+	if strings.Contains(answer, h.Expect) {
 		return true
-	} else {
-		if strings.Contains(answer, h.Expect) {
-			return true
-		}
 	}
 	return false
 }
@@ -57,10 +59,13 @@ func TcpConstructor(h Healthcheck) (HealthChecker, error) {
 	if _, ok := h.Config["port"]; !ok {
 		return TcpHealthCheck{}, errors.New("'port' not defined in tcp healthcheck config to " + h.Destination)
 	}
-	return TcpHealthCheck{
+	hc := TcpHealthCheck{
 		Destination: h.Destination,
 		Send:        "HEAD / HTTP/1.0\r\n\r\n",
-		Expect:      "200 OK",
 		Port:        h.Config["port"],
-	}, nil
+	}
+	if v, ok := h.Config["expect"]; ok {
+		hc.Expect = v
+	}
+	return hc, nil
 }
