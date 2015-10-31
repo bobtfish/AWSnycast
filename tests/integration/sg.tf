d8c517cfb1d6f5d06185af3927bb341aabde231c
Tomas Doran <tdoran@yelp.com>
2015-10-31 20:09:24 +0000

    Fix the security group

--- tests/integration/sg.tf
+++ tests/integration/sg.tf
@@ -4,10 +4,16 @@ resource "aws_security_group" "allow_all" {
   vpc_id = "${aws_vpc.main.id}"
 
   ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
       from_port = 0
       to_port = 0
       protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = ["10.0.0.0/8"]
   }
   egress {
       from_port = 0
