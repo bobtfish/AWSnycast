2d1845094df80f81dc544d2c8c8ec554b480c70a
Tomas Doran <tdoran@yelp.com>
2015-11-14 17:38:48 +0000

    Fix protocol case

--- tests/integration/sg.tf
+++ tests/integration/sg.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "allow_all" {
   ingress {
       from_port = 22
       to_port = 22
-      protocol = "TCP"
+      protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
