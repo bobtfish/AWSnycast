8ff332086e1f8d8aad6bce3869a1c7aed05cbde8
Tomas Doran <tdoran@yelp.com>
2015-11-30 18:36:58 +0000

    Fix harder

--- package/build.sh
+++ package/build.sh
@@ -4,6 +4,8 @@ project=$1
 version=$2
 iteration=$3
 
+export PATH=$PATH:/go/bin:/usr/local/go/bin
+
 cd /go/src/github.com/bobtfish/AWSnycast
 CGO_ENABLED=0 go get -a -x -installsuffix cgo -ldflags '-d -s -w' && godep go install -a -x -installsuffix cgo -ldflags '-d -s -w'
 CGO_ENABLED=0 godep go build -a -x -installsuffix cgo -ldflags '-d -s -w' .
