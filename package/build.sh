45bf8e4aa051c1791b5321dcf034b6ed3df06da2
Tomas Doran <tdoran@yelp.com>
2015-11-30 10:58:38 -0800

    Fix packaging

--- package/build.sh
+++ package/build.sh
@@ -7,12 +7,13 @@ iteration=$3
 export PATH=$PATH:/go/bin:/usr/local/go/bin
 
 cd /go/src/github.com/bobtfish/AWSnycast
-CGO_ENABLED=0 go get -a -x -installsuffix cgo -ldflags '-d -s -w' && godep go install -a -x -installsuffix cgo -ldflags '-d -s -w'
+CGO_ENABLED=0 go get
 CGO_ENABLED=0 godep go build -a -tags netgo -ldflags '-w' .
 strip AWSnycast
 mkdir /dist && cd /dist
+cp /go/bin/AWSnycast .
 fpm -s dir -t deb --name ${project} \
     --iteration ${iteration} --version ${version} \
     --prefix /usr/bin \
-    /go/bin/AWSnycast
-
+    AWSnycast
+rm AWSnycast
