e41543af3261b245b3e3964dd39b8a196e812bc5
Tomas Doran <tdoran@yelp.com>
2015-12-06 14:00:59 +0000

    Clean up test helper

--- testhelpers/multierror.go
+++ testhelpers/multierror.go
@@ -3,29 +3,23 @@ package testhelpers
 import (
 	"fmt"
 	"github.com/hashicorp/go-multierror"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func CheckOneMultiError(t *testing.T, err error, validate string) {
-	if err == nil {
-		t.Fail()
-	}
-	if merr, ok := err.(*multierror.Error); ok {
-		if len(merr.Errors) != 1 {
-			t.Log(fmt.Sprintf("%v not 1 errors", len(merr.Errors)))
-			for i, err := range merr.Errors {
-				t.Log(fmt.Sprintf("Error %v is %s", i, err.Error()))
+	if assert.NotNil(t, err) {
+		merr, ok := err.(*multierror.Error)
+		if assert.Equal(t, ok, true, "Not multierror") {
+			if len(merr.Errors) != 1 {
+				t.Log(fmt.Sprintf("%v not 1 errors", len(merr.Errors)))
+				for i, err := range merr.Errors {
+					t.Log(fmt.Sprintf("Error %v is %s", i, err.Error()))
+				}
+				t.Fail()
+				return
 			}
-			t.Fail()
-			return
+			assert.Equal(t, merr.Errors[0].Error(), validate)
 		}
-		if merr.Errors[0].Error() != validate {
-			t.Log("'" + merr.Errors[0].Error() + "' not '" + validate + "'")
-			t.Fail()
-		}
-	} else {
-		t.Log("Not multierror")
-		t.Log(err)
-		t.Fail()
 	}
 }
