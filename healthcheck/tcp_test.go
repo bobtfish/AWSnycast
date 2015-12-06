f81dbe78e39e5ef2cf554700b2d59da0eaa50b1a
Tomas Doran <tdoran@yelp.com>
2015-12-06 21:51:55 +0000

    More test cleanups

--- healthcheck/tcp_test.go
+++ healthcheck/tcp_test.go
@@ -3,6 +3,7 @@ package healthcheck
 import (
 	"fmt"
 	log "github.com/Sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 	"net"
 	"testing"
 )
@@ -18,79 +19,58 @@ func TestHealthcheckTcpNoPort(t *testing.T) {
 	}
 	h.Validate("foo", false)
 	err := h.Setup()
-	if err == nil {
-		t.Fail()
-	} else {
-		if err.Error() != "'port' not defined in tcp healthcheck config to 127.0.0.1" {
-			t.Log(err.Error())
-			t.Fail()
-		}
+	if assert.NotNil(t, err) {
+		assert.Equal(t, err.Error(), "'port' not defined in tcp healthcheck config to 127.0.0.1")
 	}
 }
 
 func TestHealthcheckTcp(t *testing.T) {
 	ln, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		t.Fatal(err)
-	}
-	port := ln.Addr().(*net.TCPAddr).Port
-	ready := make(chan bool, 1)
-	quit := false
-	go func() {
-		for {
-			ready <- true
-			conn, err := ln.Accept()
-			if err != nil {
-				if quit {
-					return
-				}
-				t.Fatal(fmt.Printf("Error accepting: %s", err.Error()))
-			}
-			go func(conn net.Conn) {
-				buf := make([]byte, 1024)
-				n, err := conn.Read(buf)
+	if assert.Nil(t, err) {
+		port := ln.Addr().(*net.TCPAddr).Port
+		ready := make(chan bool, 1)
+		quit := false
+		go func() {
+			for {
+				ready <- true
+				conn, err := ln.Accept()
 				if err != nil {
-					t.Log(fmt.Printf("Error reading: %s", err.Error()))
-					t.Fail()
+					if quit {
+						return
+					}
+					t.Fatal(fmt.Printf("Error accepting: %s", err.Error()))
 				}
-				if string(buf[:n]) != "HEAD / HTTP/1.0\r\n\r\n" {
-					t.Log(string(buf[:n]))
-					t.Fail()
-				}
-				conn.Write([]byte("200 OK"))
-				conn.Close()
-			}(conn)
+				go func(conn net.Conn) {
+					buf := make([]byte, 1024)
+					n, err := conn.Read(buf)
+					assert.Nil(t, err)
+					assert.Equal(t, string(buf[:n]), "HEAD / HTTP/1.0\r\n\r\n")
+					conn.Write([]byte("200 OK"))
+					conn.Close()
+				}(conn)
+			}
+		}()
+		<-ready
+		c := make(map[string]string)
+		c["port"] = fmt.Sprintf("%d", port)
+		c["send"] = "HEAD / HTTP/1.0\r\n\r\n"
+		c["expect"] = "200 OK"
+		h := Healthcheck{
+			Type:        "tcp",
+			Destination: "127.0.0.1",
+			Config:      c,
 		}
-	}()
-	<-ready
-	c := make(map[string]string)
-	c["port"] = fmt.Sprintf("%d", port)
-	c["send"] = "HEAD / HTTP/1.0\r\n\r\n"
-	c["expect"] = "200 OK"
-	h := Healthcheck{
-		Type:        "tcp",
-		Destination: "127.0.0.1",
-		Config:      c,
-	}
-	err = h.Validate("foo", false)
-	if err != nil {
-		t.Log(err)
-		t.Fail()
-	}
-	err = h.Setup()
-	if err != nil {
-		t.Log("Setup failed: %s", err.Error())
-		t.Fail()
-	} else {
-		log.Printf("%+v", h)
-		res := h.healthchecker.Healthcheck()
-		if !res {
-			t.Log("h.healthchecker.Healthcheck() returned false")
-			t.Fail()
+		err = h.Validate("foo", false)
+		assert.Nil(t, err)
+		err = h.Setup()
+		if assert.Nil(t, err) {
+			log.Printf("%+v", h)
+			res := h.healthchecker.Healthcheck()
+			assert.Equal(t, res, true, "h.healthchecker.Healthcheck() returned false")
 		}
+		quit = true
+		ln.Close()
 	}
-	quit = true
-	ln.Close()
 }
 
 func TestHealthcheckTcpFail(t *testing.T) {
@@ -114,14 +94,8 @@ func TestHealthcheckTcpFail(t *testing.T) {
 			go func(conn net.Conn) {
 				buf := make([]byte, 1024)
 				n, err := conn.Read(buf)
-				if err != nil {
-					t.Log(fmt.Printf("Error reading: %s", err.Error()))
-					t.Fail()
-				}
-				if string(buf[:n]) != "HEAD / HTTP/1.0\r\n\r\n" {
-					t.Log(string(buf[:n]))
-					t.Fail()
-				}
+				assert.Nil(t, err, "Error reading")
+				assert.Equal(t, string(buf[:n]), "HEAD / HTTP/1.0\r\n\r\n")
 				conn.Write([]byte("500 OOPS"))
 				conn.Close()
 			}(conn)
@@ -138,21 +112,12 @@ func TestHealthcheckTcpFail(t *testing.T) {
 		Config:      c,
 	}
 	err = h.Validate("foo", false)
-	if err != nil {
-		t.Log(err)
-		t.Fail()
-	}
+	assert.Nil(t, err)
 	err = h.Setup()
-	if err != nil {
-		t.Log("Setup failed: %s", err.Error())
-		t.Fail()
-	} else {
+	if assert.Nil(t, err) {
 		log.Printf("%+v", h)
 		res := h.healthchecker.Healthcheck()
-		if res {
-			t.Log("h.healthchecker.Healthcheck() returned OK for a 500")
-			t.Fail()
-		}
+		assert.Equal(t, res, false, "h.healthchecker.Healthcheck() returned OK for a 500")
 	}
 	quit = true
 	ln.Close()
@@ -160,152 +125,114 @@ func TestHealthcheckTcpFail(t *testing.T) {
 
 func TestHealthcheckTcpClosed(t *testing.T) {
 	ln, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		t.Fatal(err)
-	}
-	port := ln.Addr().(*net.TCPAddr).Port
-	ln.Close() // Close the port again before running healthcheck
-	c := make(map[string]string)
-	c["port"] = fmt.Sprintf("%d", port)
-	c["send"] = "HEAD / HTTP/1.0\r\n\r\n"
-	c["expect"] = "200 OK"
-	h := Healthcheck{
-		Type:        "tcp",
-		Destination: "127.0.0.1",
-		Config:      c,
-	}
-	err = h.Validate("foo", false)
-	if err != nil {
-		t.Log(err)
-		t.Fail()
-	}
-	err = h.Setup()
-	if err != nil {
-		t.Log("Setup failed: %s", err.Error())
-		t.Fail()
-	} else {
-		log.Printf("%+v", h)
-		res := h.healthchecker.Healthcheck()
-		if res {
-			t.Log("h.healthchecker.Healthcheck() returned OK for closed port")
-			t.Fail()
+	if assert.Nil(t, err) {
+		port := ln.Addr().(*net.TCPAddr).Port
+		ln.Close() // Close the port again before running healthcheck
+		c := make(map[string]string)
+		c["port"] = fmt.Sprintf("%d", port)
+		c["send"] = "HEAD / HTTP/1.0\r\n\r\n"
+		c["expect"] = "200 OK"
+		h := Healthcheck{
+			Type:        "tcp",
+			Destination: "127.0.0.1",
+			Config:      c,
+		}
+		err = h.Validate("foo", false)
+		assert.Nil(t, err)
+		err = h.Setup()
+		if assert.Nil(t, err) {
+			log.Printf("%+v", h)
+			assert.Equal(t, h.healthchecker.Healthcheck(), false, "h.healthchecker.Healthcheck() returned OK for closed port")
 		}
 	}
 }
 
 func TestHealthcheckTcpFailClientClose(t *testing.T) {
 	ln, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		t.Fatal(err)
-	}
-	port := ln.Addr().(*net.TCPAddr).Port
-	ready := make(chan bool, 1)
-	quit := false
-	go func() {
-		for {
-			ready <- true
-			conn, err := ln.Accept()
-			if err != nil {
-				if quit {
-					return
+	if assert.Nil(t, err) {
+		port := ln.Addr().(*net.TCPAddr).Port
+		ready := make(chan bool, 1)
+		quit := false
+		go func() {
+			for {
+				ready <- true
+				conn, err := ln.Accept()
+				if err != nil {
+					if quit {
+						return
+					}
+					t.Fatal(fmt.Printf("Error accepting: %s", err.Error()))
 				}
-				t.Fatal(fmt.Printf("Error accepting: %s", err.Error()))
+				conn.Close() // Client closes connection straight away, before reading anything
 			}
-			conn.Close() // Client closes connection straight away, before reading anything
+		}()
+		<-ready
+		c := make(map[string]string)
+		c["port"] = fmt.Sprintf("%d", port)
+		c["send"] = "HEAD / HTTP/1.0\r\n\r\n"
+		c["expect"] = "200 OK"
+		h := Healthcheck{
+			Type:        "tcp",
+			Destination: "127.0.0.1",
+			Config:      c,
 		}
-	}()
-	<-ready
-	c := make(map[string]string)
-	c["port"] = fmt.Sprintf("%d", port)
-	c["send"] = "HEAD / HTTP/1.0\r\n\r\n"
-	c["expect"] = "200 OK"
-	h := Healthcheck{
-		Type:        "tcp",
-		Destination: "127.0.0.1",
-		Config:      c,
-	}
-	err = h.Validate("foo", false)
-	if err != nil {
-		t.Log(err)
-		t.Fail()
-	}
-	err = h.Setup()
-	if err != nil {
-		t.Log("Setup failed: %s", err.Error())
-		t.Fail()
-	} else {
-		log.Printf("%+v", h)
-		res := h.healthchecker.Healthcheck()
-		if res {
-			t.Log("h.healthchecker.Healthcheck() returned OK for client close before send")
-			t.Fail()
+		err = h.Validate("foo", false)
+		assert.Nil(t, err)
+		err = h.Setup()
+		if assert.Nil(t, err) {
+			log.Printf("%+v", h)
+			res := h.healthchecker.Healthcheck()
+			assert.Equal(t, res, false, "h.healthchecker.Healthcheck() returned OK for client close before send")
 		}
+		quit = true
+		ln.Close()
 	}
-	quit = true
-	ln.Close()
 }
 
 func TestHealthcheckTcpNoExpect(t *testing.T) {
 	ln, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		t.Fatal(err)
-	}
-	port := ln.Addr().(*net.TCPAddr).Port
-	ready := make(chan bool, 1)
-	quit := false
-	go func() {
-		for {
-			ready <- true
-			conn, err := ln.Accept()
-			if err != nil {
-				if quit {
-					return
-				}
-				t.Fatal(fmt.Printf("Error accepting: %s", err.Error()))
-			}
-			go func(conn net.Conn) {
-				buf := make([]byte, 1024)
-				n, err := conn.Read(buf)
+	if assert.Nil(t, err) {
+		port := ln.Addr().(*net.TCPAddr).Port
+		ready := make(chan bool, 1)
+		quit := false
+		go func() {
+			for {
+				ready <- true
+				conn, err := ln.Accept()
 				if err != nil {
-					t.Log(fmt.Printf("Error reading: %s", err.Error()))
-					t.Fail()
+					if quit {
+						return
+					}
+					t.Fatal(fmt.Printf("Error accepting: %s", err.Error()))
 				}
-				if string(buf[:n]) != "HEAD / HTTP/1.0\r\n\r\n" {
-					t.Log(string(buf[:n]))
-					t.Fail()
-				}
-				conn.Close()
-			}(conn)
+				go func(conn net.Conn) {
+					buf := make([]byte, 1024)
+					n, err := conn.Read(buf)
+					assert.Nil(t, err)
+					assert.Equal(t, string(buf[:n]), "HEAD / HTTP/1.0\r\n\r\n")
+					conn.Close()
+				}(conn)
+			}
+		}()
+		<-ready
+		c := make(map[string]string)
+		c["port"] = fmt.Sprintf("%d", port)
+		c["send"] = "HEAD / HTTP/1.0\r\n\r\n"
+		h := Healthcheck{
+			Type:        "tcp",
+			Destination: "127.0.0.1",
+			Config:      c,
 		}
-	}()
-	<-ready
-	c := make(map[string]string)
-	c["port"] = fmt.Sprintf("%d", port)
-	c["send"] = "HEAD / HTTP/1.0\r\n\r\n"
-	h := Healthcheck{
-		Type:        "tcp",
-		Destination: "127.0.0.1",
-		Config:      c,
-	}
-	err = h.Validate("foo", false)
-	if err != nil {
-		t.Log(err)
-		t.Fail()
-	}
-	err = h.Setup()
-	if err != nil {
-		t.Log("Setup failed: %s", err.Error())
-		t.Fail()
-	} else {
-		log.Printf("%+v", h)
-		res := h.healthchecker.Healthcheck()
-		if !res {
-			t.Log("h.healthchecker.Healthcheck() returned false")
-			t.Fail()
+		err = h.Validate("foo", false)
+		assert.Nil(t, err)
+		err = h.Setup()
+		if assert.Nil(t, err) {
+			log.Printf("%+v", h)
+			assert.Equal(t, h.healthchecker.Healthcheck(), true)
 		}
+		quit = true
+		ln.Close()
 	}
-	quit = true
-	ln.Close()
 }
 
 func TestHealthcheckTcpNoSendOrExpect(t *testing.T) {
