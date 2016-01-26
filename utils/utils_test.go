ad133e6808dde6e49a1a4a8fc279fe6d55d9c6e1
T SHEASHA <tsheasha@yelp.com>
2016-01-26 06:42:59 -0800

    Fix tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -11,19 +11,19 @@ func TestGetBool(t *testing.T) {
 
 	val, err := GetAsBool("false", true)
 	assert.Equal(val, false)
-	assert.Nil(err)
+	assert.Nil(t, err)
 
 	val, err = GetAsBool("notabool", false)
 	assert.Equal(val, false)
-	assert.NotNil(err)
+	assert.NotNil(t, err)
 
 	val, err = GetAsBool(true, false)
 	assert.Equal(val, true)
-	assert.Nil(err)
+	assert.Nil(t, err)
 
 	val, err = GetAsBool("True", false)
 	assert.Equal(val, true)
-	assert.Nil(err)
+	assert.Nil(t, err)
 }
 
 func TestGetInt(t *testing.T) {
@@ -31,19 +31,19 @@ func TestGetInt(t *testing.T) {
 
 	val, err := GetAsInt("10", 123)
 	assert.Equal(val, 10)
-	assert.Nil(err)
+	assert.Nil(t, err)
 
 	val, err = GetAsInt("notanint", 123)
 	assert.Equal(val, 123)
-	assert.NotNil(err)
+	assert.NotNil(t, err)
 
 	val, err = GetAsInt(12.123, 123)
 	assert.Equal(val, 12)
-	assert.Nil(err)
+	assert.Nil(t, err)
 
 	val, err = GetAsInt(12, 123)
 	assert.Equal(val, 12)
-	assert.Nil(err)
+	assert.Nil(t, err)
 }
 
 func TestGetFloat(t *testing.T) {
@@ -51,19 +51,19 @@ func TestGetFloat(t *testing.T) {
 
 	val, err := GetAsFloat("10", 123)
 	assert.Equal(val, 10.0)
-	assert.Nil(err)
+	assert.Nil(t, err)
 
 	val, err = GetAsFloat("10.21", 123)
 	assert.Equal(val, 10.21)
-	assert.Nil(err)
+	assert.Nil(t, err)
 
-	val, err = confir.GetAsFloat("notafloat", 123)
+	val, err = GetAsFloat("notafloat", 123)
 	assert.Equal(val, 123.0)
-	assert.NotNil(err)
+	assert.NotNil(t, err)
 
 	val, err = GetAsFloat(12.123, 123)
 	assert.Equal(val, 12.123)
-	assert.Nil(err)
+	assert.Nil(t, err)
 }
 
 func TestGetString(t *testing.T) {
@@ -73,10 +73,10 @@ func TestGetString(t *testing.T) {
 	assert.Equal(val, "10")
 
 	val = GetAsString(10)
-	assert.equal(val, "10")
+	assert.Equal(val, "10")
 
 	val = GetAsString(10.123)
-	assert.equal(val, "10.123")
+	assert.Equal(val, "10.123")
 }
 
 func TestGetAsMap(t *testing.T) {
@@ -88,18 +88,19 @@ func TestGetAsMap(t *testing.T) {
 		"runtimeenv": "dev",
 		"region":     "uswest1-devc",
 	}
-	assert.Equal(GetAsMap(stringToParse), expectedValue)
+	actualValue, err := GetAsMap(stringToParse)
+	assert.Equal(actualValue, expectedValue)
 
 	// Test if map[string]interface{} can be converted to map[string]string
 	interfaceMapToParse := make(map[string]interface{})
 	interfaceMapToParse["foo"] = "bar"
 	interfaceMapToParse["alice"] = "bob"
 
-	actualValue, err := GetAsMap(interfaceMapToParse)
+	actualValue, err = GetAsMap(interfaceMapToParse)
 	assert.Equal(actualValue, expectedValue)
 
 	actualValue, err = GetAsMap(123)
-	assert.NotNil(err)
+	assert.NotNil(t, err)
 }
 
 func TestGetAsSlice(t *testing.T) {
@@ -108,14 +109,15 @@ func TestGetAsSlice(t *testing.T) {
 	// Test if string array can be converted to []string
 	stringToParse := "[\"baz\", \"bat\"]"
 	expectedValue := []string{"baz", "bat"}
-	assert.Equal(GetAsSlice(stringToParse), expectedValue)
+	actualValue, err := GetAsSlice(stringToParse)
+	assert.Equal(actualValue, expectedValue)
 
 	sliceToParse := []string{"baz", "bat"}
-	actualValue, err := GetAsSlice(sliceToParse)
+	actualValue, err = GetAsSlice(sliceToParse)
 	assert.Equal(actualValue, expectedValue)
 
 	actualValue, err = GetAsSlice(123)
-	assert.NotNil(err)
+	assert.NotNil(t, err)
 }
 
 func TestGetAsSliceFromYAML(t *testing.T) {
@@ -132,6 +134,6 @@ func TestGetAsSliceFromYAML(t *testing.T) {
 		assert.Equal(t, []string{"a", "b", "c"}, res)
 
 		res, err = GetAsSlice(123)
-		assert.NotNil(err)
+		assert.NotNil(t, err)
 	}
 }
