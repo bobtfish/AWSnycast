2093d8615ca87493e42b3700e79f7fa7d1fc8201
Tomas Doran <bobtfish@bobtfish.net>
2016-03-05 10:14:16 +0000

    Stop the command healthcheck test from panicing

--- healthcheck/command_test.go
+++ healthcheck/command_test.go
@@ -6,23 +6,31 @@ import (
 )
 
 func TestHealthcheckCommand(t *testing.T) {
+	c := make(map[string]interface{})
+	c["command"] = "/bin/true"
 	h := Healthcheck{
 		Type:        "command",
 		Destination: "127.0.0.1",
+		Config: c,
 	}
 	err := h.Validate("foo", false)
-	assert.Nil(t, err)
-	h.Setup()
-	assert.Equal(t, h.healthchecker.Healthcheck(), true)
+	if assert.Nil(t, err) {
+		h.Setup()
+		assert.Equal(t, h.healthchecker.Healthcheck(), true)
+	}
 }
 
 func TestHealthcheckCommandFail(t *testing.T) {
+	c := make(map[string]interface{})
+        c["command"] = "/bin/false"
 	h := Healthcheck{
 		Type:        "command",
 		Destination: "169.254.255.45", // Hopefully you can't talk to this :)
+		Config: c,
 	}
 	err := h.Validate("foo", false)
-	assert.Nil(t, err)
-	h.Setup()
-	assert.Equal(t, h.healthchecker.Healthcheck(), false)
+	if assert.Nil(t, err) {
+		h.Setup()
+		assert.Equal(t, h.healthchecker.Healthcheck(), false)
+	}
 }
